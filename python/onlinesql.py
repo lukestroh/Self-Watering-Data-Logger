190b95da4c223000fa8053851758e2305463069d
lukestroh <66746219+lukestroh@users.noreply.github.com>
2020-09-11 17:23:45 -0400

    Update onlinesql.py

--- python/onlinesql.py
+++ python/onlinesql.py
@@ -7,8 +7,6 @@ class mySequel:
             user = "username",
             password = "password",
             database = "database_name",
-            # port = 3306
-            # auth_plugin = "mysql_native_password"
         )
         return mydb
 
@@ -28,25 +26,4 @@ class mySequel:
         database.ping(True)
         cursor.execute(query, values)
         database.ping(True)
-        database.commit() # This still doesn't work for gardenBTSerialReader, but it worked in prompt!
-
-
-
-# """ Creating the table""" # "UNIQUE" statement after unix_time makes sure two duplicate entries are not added
-# cursor.execute("CREATE TABLE garden_data (unix_time VARCHAR(10) UNIQUE, date VARCHAR(25), soil_temp VARCHAR(6), soil_moisture VARCHAR(6), air_temp VARCHAR(6), air_humidity VARCHAR(6), heat_index VARCHAR(6), sunlight VARCHAR(6), watered_today VARCHAR(6))")
-
-
-
-# Example query add:
-# query = "INSERT IGNORE INTO garden_data (unix_time, date, soil_temp, soil_moisture, air_temp, air_humidity, heat_index, sunlight, watered_today) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-# values = ("1597777777","8/3/2020 3:53:58 PM","27.19","0.36","26.5","60.4","27.5","73.63","0")
-# cursor.execute(query, values)
-
-
-# Required to make changes
-# db.commit()
-
-# cursor.execute("SELECT * FROM garden_data")
-# records = cursor.fetchall()
-# for record in records:
-#     print(record)
+        database.commit()
