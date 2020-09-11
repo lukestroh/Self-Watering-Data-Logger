9425e4b61743bd4ca4d49ecab8912f51db353515
lukestroh <66746219+lukestroh@users.noreply.github.com>
2020-09-11 15:51:10 -0400

    Add files via upload

--- python/gardenBTSerialReader.py
+++ python/gardenBTSerialReader.py
@@ -1,120 +1,177 @@
-import serial, sys, csv, time
-import matplotlib
-matplotlib.use("tkAgg")
-import matplotlib.pyplot as plt
+import serial, time
 import numpy as np
 
+import onlinesql as ons
 
 
+port_timeout = 1260 # 21 minutes, in case we water.
+HC06_port = "COM16"
+ticker = 0
+data_file = "test_data.csv"
+
+class ReadingDone(Exception):
+    pass
+
+# Connect to port, keep trying if it doesn't work
+def connectToPort():
+    while True:
+        try:
+            ser = serial.Serial(HC06_port, timeout = port_timeout)
+            print("Connected to Bluetooth.")
+            return ser
+        except serial.serialutil.SerialException:
+            print("An error occurred while attempting to connect. Retrying...\n")
+        continue
 
 
+# Update the last unix time data received from the Arduino to txt file.
+def updateLastEntry(last_entry):
+    with open("last_entry.txt","w", newline='') as last_entry_file:
+        last_entry_file.write(last_entry)
+        last_entry_file.close()
+        time.sleep(0.001)
 
+# Get the last entry, with two returns. One for Python to use to compare, the other to send, with markers, to the Arduino
 def getLastEntry():
     with open("last_entry.txt","r") as last_entry_file:
         last_entry = last_entry_file.read()
-        return last_entry
-
-def updateLastEntry(last_entry):
-    with open("last_entry.txt","w", newline='') as last_entry_file:
-        # lines = last_entry_file.readlines()
-        # lines[0] = last_entry
-        last_entry_file.write(last_entry)
-        last_entry_file.close()
+        last_entry_markers = '<' + last_entry + '>'
+        return last_entry, last_entry_markers
 
 
-# Declare and try to open port with limited attempts
-def connectToPort():
-    connect_attempts = 10
-    HC06_port = 'COM16'
+# Read from the serial port, save data in an array
+def readSerialLines(ser):
+    # start a timer for the readline command
+    t0 = time.time()
+    # read bytes from serial port
+    ser_bytes = ser.readline()
+    t1 = time.time()
+    reading_time = t1-t0
+    # if timer exceeds logging time, as it will when we are not connected, close the port and break from loop
+    if reading_time >= port_timeout:
+        ser.close()        # closing the port also deletes
+        raise ReadingDone  # Raise error to break from while loop in getData() so that we can attempt to reconnect to port
 
-    # for i in range(0, connect_attempts):
+    decoded_bytes = (ser_bytes[0:len(ser_bytes)-2].decode("utf-8",errors='replace')) # replaces any errors with a '?'
+    decoded_array = np.array(decoded_bytes.split(',')) # Separate my comma-separated string, store in array since the csv writer can only deal with commas this way
+
+    print(decoded_bytes)
+
+    time.sleep(0.01)
+    return decoded_array
+
+
+def getData(ser, database, dbcursor):
     while True:
         try:
-            ser = serial.Serial(HC06_port, baudrate = 9600)
-            ser.flushInput()
-            return ser
-        except serial.serialutil.SerialException:
-                # print("An exception occurred while trying to create a serial connection. Trying again... (Attempts left: {number})\n".format(number = connect_attempts - (i+1)))
-                print("An exception occurred while trying to create a serial connection. Trying again...\n")
-                continue
-        break
+            # Send the unix time of the last piece of data we got
+            sendData(ser)
+
+            decoded_array = readSerialLines(ser)
+
+
+            # If the data sent by the Arduino is "False," we know we're going to be updated by the backup. We don't save this line to the file
+            if decoded_array[0] == "False":
+                # The next data sent is the last unix time stored by the Arudino. This is a reference number and we want to save it in arduino_unix, but we don't want to save to file.
+                decoded_array = readSerialLines(ser)
+
+                # Store the next entry, the "char_unix", into a variable
+                arduino_unix = decoded_array[0]
+
+                # Print the incoming data from the SD card.
+                print("From SD card backup:")
+                decoded_array = readSerialLines(ser) # This next line gets the first data line from the SD card. We know it will be the header line, so we will definitely enter the following while loop. We DO NOT want this uploaded to the database.
+
+
+                while decoded_array[0] != arduino_unix and arduino_unix != '':
+                    decoded_array = readSerialLines(ser)
+
+                    # writeToFile(data_file, decoded_array)
+                    ons.mySequel.addLine(database, dbcursor, decoded_array)
+                    print("check")
+                ticker = 1
+
+                print("Live data stream:")
+
+            # If the Arduino had to send old data, then we still have to get the next live data from the Arduino without sending it the old data from 6 min ago using sendData().
+            if ticker == 1:
+                decoded_array = readSerialLines(ser)
+                ticker = 0
+
+            time.sleep(0.01)
+
+            updateLastEntry(decoded_array[0]) # store the latest Unix time value into a text file. That way, we have a 'key' to send the Arduino
+            # writeToFile(data_file, decoded_array)
+
+            # Sending data to the database
+            ons.mySequel.addLine(database, dbcursor, decoded_array)
 
 
 
-
-def sendData(ser):
-    last_entry = getLastEntry()
-    encoded_last_entry = str.encode(last_entry)
-    ser.write(encoded_last_entry)
-
-
-# # make a plot
-# plot_window = 20
-# y_var = np.array(np.zeros([plot_window]))
-#
-# plt.ion()
-# fig, ax = plt.subplots()
-# line, = ax.plot(y_var)
-
-def getData(ser):
-    while True:
-        try:
-            # while ser.in_waiting:
-            ser_bytes = ser.readline()
-            decoded_bytes = (ser_bytes[0:len(ser_bytes)-2].decode("utf-8",errors='replace')) # replaces any errors with a '?'
-            print(decoded_bytes)
-            updateLastEntry(decoded_bytes)
-
-            with open("test_data.csv","a", newline='') as f:
-                """
-                'a' tells Python to append the serial port data and ensure that no data is erased in the existing file. This prevents overwriting.
-
-                newline='' makes sure we don't have a space between rows
-
-
-                Note: Opening the .csv file elsewhere will stop the writing process. Perhaps this is why matplotlib isn't working?
-                """
-                writer = csv.writer(f,delimiter=',')
-                writer.writerow([decoded_bytes])
-            # y_var = np.append(y_var, decoded_bytes)
-            # y_var = y_var[1:plot_window+1]
-            # line.set_ydata(y_var)
-            # ax.relim()
-            # ax.autoscale_view()
-            # fig.canvas.draw()
-            # fig.canvas.flush_events()
-
-        except:
-            print("Keyboard Interrupt")
+        except ReadingDone:
+            print("Port Timeout")
             break
 
 
+# Get the last unix time from your saved file and send to the Arduino.
+def sendData(ser):
+    last_entry = getLastEntry()
+    ser.write(str.encode(last_entry[1]))
+    print("Sending data: " + last_entry[0])
 
-def userInputter(ser):
-    while True:
-        while ser.in_waiting:
-            ser_bytes = ser.readline()
-            decoded_bytes = (ser_bytes[0:len(ser_bytes)-2].decode("utf-8",errors='replace')) # replaces any errors with a '?'
-            print(decoded_bytes)
-        user_command = input("'1' to turn on LED, '0' to turn in off!")
-        usr_command_bytes = str.encode(user_command)
-        ser.write(usr_command_bytes)
-        time.sleep(0.5)
+
+
+"""
+
+Notes:
+
+Something is still seriously wrong with updateLastEntry(), or something in that process. Occasionally, Python doesn't seem to update this, for some unknown reason. Perhaps we need to check the logical flow after the SD card backup?
+
+Watering and then updating the next bit of data seems to take 12 minutes, according to the Arduino time stamp. Why?
+8/31/20 ~9:09PM Arduino is watering again?????? (Red LED is lit)
+    perhaps the arduino restarted?
+
+    edit: without any noticible restart, the arduino is watering again. Perhaps check the number of files on SD card to examine restarts? ALSO: evidence for restart - "From SD card backup" only send the header line, and no data. This certainly indicates that something is wrong.
+
+    answer: the loop has to wait for its elapsed time. The loop delay is based on the modulo of millis, and with a 10 minute watering time, we have ended up going 4 minutes past what the expected modulo time would be (roughly). This is why we get our data 2 min later.
+    What would happen if we watered first, then got data?
+
+
+"""
+
+# def userInputter(ser):
+#     while True:
+#         while ser.in_waiting:
+#             ser_bytes = ser.readline()
+#             decoded_bytes = (ser_bytes[0:len(ser_bytes)-2].decode("utf-8",errors='replace')) # replaces any errors with a '?'
+#             print(decoded_bytes)
+#         user_command = input("'1' to turn on LED, '0' to turn in off!")
+#         usr_command_bytes = str.encode(user_command)
+#         ser.write(usr_command_bytes)
+#         time.sleep(0.5)
 
 
 
 def main():
+    # Connect to databse
+    db = ons.mySequel.connectToDB()
+    db.ping(True)
+    print(db)
+    cursor = db.cursor()
+
+    # Connect to Bluetooth Serial Port
     connection = connectToPort()
 
-
-    # userInputter(connection)
-
-
-
-    # sendData(connection)
-    # if sendData returns a true, then proceed as normal
-    getData(connection)
-    # elseif sendData returns a false, get all the next data and save last line
+    while True:
+        try:
+            # if the port gets closed, reconnect
+            while connection.is_open == False:
+                connection = connectToPort()
+            getData(connection, db, cursor)
+            # if KeyboardInterrupt: # Serial timeout triggers KeyboardInterrupt, making it not possible to include this in code
+            #     break
+        except:
+            continue
 
 if __name__ == "__main__":
     main()
---
+++ python/onlinesql.py
@@ -0,0 +1,57 @@
+# // database: u463618153_garden_databas
+# // user: u463618153_quescondido
+# // host: sql173.main-hosting.eu
+# // password: 123dbPasswords
+
+import mysql.connector
+
+class mySequel:
+    def connectToDB():
+        mydb = mysql.connector.connect(
+            host = "sql173.main-hosting.eu", # This is the domain where the database is held
+            user = "u463618153_quescondido",
+            password = "123dbPasswords",
+            database = "u463618153_garden_databas",
+            # port = 3306
+            # auth_plugin = "mysql_native_password"
+        )
+        return mydb
+
+    def addLine(database, cursor, data_array):
+        query = "INSERT IGNORE INTO garden_data (unix_time, date, soil_temp, soil_moisture, air_temp, air_humidity, heat_index, sunlight, watered_today) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        values = (
+            data_array[0],
+            data_array[1],
+            data_array[2],
+            data_array[3],
+            data_array[4],
+            data_array[5],
+            data_array[6],
+            data_array[7],
+            data_array[8]
+        )
+        database.ping(True)
+        cursor.execute(query, values)
+        database.ping(True)
+        database.commit() # This still doesn't work for gardenBTSerialReader, but it worked in prompt!
+
+
+
+# """ Creating the table""" # "UNIQUE" statement after unix_time makes sure two duplicate entries are not added
+# cursor.execute("CREATE TABLE garden_data (unix_time VARCHAR(10) UNIQUE, date VARCHAR(25), soil_temp VARCHAR(6), soil_moisture VARCHAR(6), air_temp VARCHAR(6), air_humidity VARCHAR(6), heat_index VARCHAR(6), sunlight VARCHAR(6), watered_today VARCHAR(6))")
+
+
+
+# Example query add:
+# query = "INSERT IGNORE INTO garden_data (unix_time, date, soil_temp, soil_moisture, air_temp, air_humidity, heat_index, sunlight, watered_today) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+# values = ("1597777777","8/3/2020 3:53:58 PM","27.19","0.36","26.5","60.4","27.5","73.63","0")
+# cursor.execute(query, values)
+
+
+# Required to make changes
+# db.commit()
+
+# cursor.execute("SELECT * FROM garden_data")
+# records = cursor.fetchall()
+# for record in records:
+#     print(record)
